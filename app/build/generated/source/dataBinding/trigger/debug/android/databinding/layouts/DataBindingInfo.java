67b3962c9339ab60aab428d9457772ade7bd72cb
Ross Centanni <ross.centanni@mobileappscompany.us>
2018-06-14 20:30:33 -0400

    Codelab complete.

---
+++ app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="cdd606e2-7c95-4d14-865c-5dedce5f60c2")
+public class DataBindingInfo {}
